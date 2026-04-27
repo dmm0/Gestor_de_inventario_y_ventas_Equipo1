eb32e6280d04d0d0b64f4037a937a0db227cfadc
her45 <her45@JOHANAPARK>
2026-04-27 08:13:37 -0600

    Module actualizado

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module com.mycompany.ingsoftware {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.desktop;
-    requires java.logging;
-    requires java.base;
     requires java.sql;
+    requires java.desktop; // Para iconos o componentes AWT si los usas
     
+    // Abrir el paquete de controladores para que JavaFX pueda leerlos
     opens controladores to javafx.fxml;
     
+    // Exportar el paquete para que sea accesible
     exports controladores;
-    
 }
